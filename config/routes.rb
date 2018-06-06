0db3dbd90ed08e3e6397afde4551297bb93e189f
Alison <lagerlof.alison@gmail.com>
2018-06-06 20:05:29 +0200

    maj mission v2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :benevoles
   devise_for :assos
 
-  root to: 'missions#index'
+  root to: 'mission#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
