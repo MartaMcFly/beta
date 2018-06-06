f60e35b8609b3ea9a3ab3bfe39843efe54a7f2f9
Alison <lagerlof.alison@gmail.com>
2018-06-06 19:53:42 +0200

    rooting test

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :missions
   devise_for :benevoles
   devise_for :assos
+
+  root to: 'missions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
